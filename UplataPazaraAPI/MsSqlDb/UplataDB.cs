3ea737da43ecf4e256d69bfbff6cdf1161b44d41
KRIS <kristijan.petkovic@hotmail.com>
2022-01-29 23:47:11 +0100

    connection string

--- UplataPazaraAPI/MsSqlDb/UplataDB.cs
+++ UplataPazaraAPI/MsSqlDb/UplataDB.cs
@@ -25,7 +25,7 @@ namespace UplataPazaraAPI.MsSqlDb
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                //Connection string bi trebalo sakriti
                 optionsBuilder.UseSqlServer("Data Source=KRIS\\SQLDEVELOPER2014;Initial Catalog=UplataPazaraDB;Integrated Security=true");
             }
         }
